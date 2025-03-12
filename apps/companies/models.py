92ed5289787a703c3bf18d4ff42326d82d2edfef
Hossam <hossam.mohamed@shelter-technology.com>
2025-03-12 21:43:39 +0200

    update phone and email to be nullable

--- apps/companies/models.py
+++ apps/companies/models.py
@@ -4,8 +4,10 @@ from django_extensions.db.models import TimeStampedModel
 
 class Company(TimeStampedModel):
     name = models.CharField(max_length=255)
-    email = models.EmailField(max_length=255)
-    phone_number = models.CharField(max_length=11)
+    address = models.CharField(max_length=255)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    email = models.EmailField(max_length=255, null=True, blank=True)
+    phone_number = models.CharField(max_length=11, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -13,8 +15,8 @@ class Company(TimeStampedModel):
 
 class CompanyBranch(TimeStampedModel):
     name = models.CharField(max_length=255)
-    email = models.EmailField(max_length=255)
-    phone_number = models.CharField(max_length=11)
+    email = models.EmailField(max_length=255, null=True, blank=True)
+    phone_number = models.CharField(max_length=11, null=True, blank=True)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
 
     def __str__(self):
