efb7449b9d87b11e9687884331fb14ea63a4617d
Hossam <hossam.mohamed@shelter-technology.com>
2025-03-13 21:03:00 +0200

    add admin panel for geo app

--- apps/geo/admin.py
+++ apps/geo/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Country, City, District
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code')
 
-# Register your models here.
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country')
+
+class DistrictAdmin(admin.ModelAdmin):
+    list_display = ('name', 'city')
+
+admin.site.register(Country, CountryAdmin)
+admin.site.register(City, CityAdmin)
+admin.site.register(District, DistrictAdmin)
