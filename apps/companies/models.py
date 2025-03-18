8ce4cddb511f4de4dafda48277d039c53dec7f54
Hossam <hossam.mohamed@shelter-technology.com>
2025-03-18 14:35:51 +0200

    add address in company branch

--- apps/companies/models.py
+++ apps/companies/models.py
@@ -24,6 +24,7 @@ class CompanyBranch(TimeStampedModel):
     name = models.CharField(max_length=255)
     email = models.EmailField(max_length=255, null=True, blank=True)
     phone_number = models.CharField(max_length=11, null=True, blank=True)
+    address = models.CharField(max_length=255, null=True, blank=True)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     district = models.ForeignKey('geo.District', on_delete=models.SET_NULL, null=True, blank=True)
 
