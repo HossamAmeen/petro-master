60a5926043666a7be7df595fd5cd53fce65dc49d
Hossam <hossam.mohamed@shelter-technology.com>
2025-04-06 23:46:48 +0200

    add default value for status in company cash request

--- apps/companies/models/company_cash_models.py
+++ apps/companies/models/company_cash_models.py
@@ -13,7 +13,7 @@ class CompanyCashRequest(AbstractBaseModel):
         REJECTED = 'Rejected'
 
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    status = models.CharField(max_length=20, choices=Status.choices)
+    status = models.CharField(max_length=20, choices=Status.choices, default=Status.PENDING)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     driver = models.ForeignKey(Driver, on_delete=models.CASCADE)
     station = models.ForeignKey(Station, on_delete=models.SET_NULL, null=True, blank=True)
