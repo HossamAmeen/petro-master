c073c499fa656adf94471c6f97e1e55c0abb0490
Hossam <hossam.mohamed@shelter-technology.com>
2025-04-18 19:58:04 +0200

    add district with city name in list stations

--- apps/stations/v1/serializers.py
+++ apps/stations/v1/serializers.py
@@ -1,17 +1,19 @@
 from rest_framework import serializers
 
+from apps.geo.v1.serializers import DistrictWithcitynameSerializer
 from apps.stations.models.stations_models import Station, StationService
 
 
 class SingleStationServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = StationService
-        fields = ['id', 'service']
+        fields = ["id", "service"]
 
 
 class ListStationSerializer(serializers.ModelSerializer):
-    services = SingleStationServiceSerializer(source='station_services', many=True)
+    services = SingleStationServiceSerializer(source="station_services", many=True)
+    district = DistrictWithcitynameSerializer()
 
     class Meta:
         model = Station
-        fields = '__all__'
+        fields = "__all__"
