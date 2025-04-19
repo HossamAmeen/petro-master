63bb6d19514345e2131c6cf90aef829ad64b58fa
Hossam <hossam.mohamed@shelter-technology.com>
2025-04-19 19:24:28 +0200

    Add static in urls

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,8 @@
 URL configuration for config project.
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import (
@@ -31,4 +33,4 @@ urlpatterns = [
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
