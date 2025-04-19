6a5cb1f5c8985c3172ef6b62317b144fc251c8d8
Hossam <hossam.mohamed@shelter-technology.com>
2025-04-19 19:31:25 +0200

    return service name in list stations

--- apps/stations/v1/serializers.py
+++ apps/stations/v1/serializers.py
@@ -1,17 +1,26 @@
 from rest_framework import serializers
 
 from apps.geo.v1.serializers import DistrictWithcitynameSerializer
-from apps.stations.models.stations_models import Station, StationService
+from apps.stations.models.stations_models import Service, Station, StationService
+
+
+class ServiceNameSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Service
+        fields = ["id", "name"]
 
 
 class SingleStationServiceSerializer(serializers.ModelSerializer):
+    service = ServiceNameSerializer()
+
     class Meta:
         model = StationService
         fields = ["id", "service"]
 
 
 class ListStationSerializer(serializers.ModelSerializer):
-    services = SingleStationServiceSerializer(source="station_services", many=True)
+    services = SingleStationServiceSerializer(many=True)
     district = DistrictWithcitynameSerializer()
 
     class Meta:
