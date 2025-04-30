57202b009e20ff336bcaef9f8ef9939ca1e4ae49
safaamostafa <safamos21@gmail.com>
2025-04-30 15:31:20 +0300

    update station view file

--- apps/users/v1/views/station_view.py
+++ apps/users/v1/views/station_view.py
@@ -1,6 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, viewsets
-from rest_framework.permissions import AllowAny
 
 from apps.users.models import StationOwner
 from apps.users.v1.serializers.station_serializer import (
@@ -11,7 +10,6 @@ from apps.users.v1.serializers.station_serializer import (
 
 class StationOwnerViewSet(viewsets.ModelViewSet):
     queryset = StationOwner.objects.select_related("station").order_by("-id")
-    permission_classes = [AllowAny]
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = ["station"]
     search_fields = ["name", "phone_number", "email"]
