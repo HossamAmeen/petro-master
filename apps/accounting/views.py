71fc079f10a26e64e27b5079df314d601d334d9c
Atareq <97070266+Atareq@users.noreply.github.com>
2025-05-05 12:06:28 +0300

    feature/accounting-views-and-serializer (#12)

--- apps/accounting/views.py
+++ apps/accounting/views.py
@@ -1,2 +1,11 @@
-# TODO: i will add for 'KhaznaTransaction.reviewd_by' and this how will the data looks
-#   {"id": 1, "username": "ahmed", "timestamp": "2025-04-20T17:30Z"},
+from rest_framework import viewsets
+from .models import KhaznaTransaction
+from .serializers import KhaznaTransactionSerializer
+from rest_framework.permissions import IsAuthenticated
+
+
+class KhaznaTransactionViewSet(viewsets.ModelViewSet):
+    queryset = KhaznaTransaction.objects.all()
+    serializer_class = KhaznaTransactionSerializer
+    permission_classes = [IsAuthenticated]
+
