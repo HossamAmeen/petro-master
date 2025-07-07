bd2db286803e991fdec49f036ea972ff1bd1470b
Hossam <hossam.mohamed@shelter-technology.com>
2025-07-07 17:54:27 +0300

    dashboard to list branches

--- apps/companies/api/v1/views/company_views.py
+++ apps/companies/api/v1/views/company_views.py
@@ -77,7 +77,7 @@ class CompanyBranchViewSet(InjectUserMixin, viewsets.ModelViewSet):
 
     def get_permissions(self):
         if self.action == "list":
-            return [CompanyPermission(), DashboardPermission()]
+            return [CompanyPermission() | DashboardPermission()]
         if self.action == "assign_managers":
             return [CompanyOwnerPermission()]
         if self.action == "update_balance":
