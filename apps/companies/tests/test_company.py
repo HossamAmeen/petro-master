12aeaec11ca51b4cdbe002b64f85b9b14d90dfd3
safaamostafa <safamos21@gmail.com>
2025-07-14 19:57:14 +0300

    remove comment fome  test list company branch

--- apps/companies/tests/test_company.py
+++ apps/companies/tests/test_company.py
@@ -12,45 +12,45 @@ from apps.users.models import User
 os.environ["SENDGRID_API_KEY"] = "fake-key-for-tests"
 
 
-# @pytest.mark.django_db
-# class TestCompany:
-#     def test_list_companies_success(self):
-#         url = reverse("companies-list")
-#         admin = User.objects.create(
-#             role=User.UserRoles.Admin,
-#             email="admin@gmail.com",
-#             name="admin",
-#             phone_number="123456789",
-#         )
-#         admin.set_password("admin")
-#         admin.save()
-#         access_token = AccessToken.for_user(admin)
-#         client = APIClient()
-#         client.credentials(HTTP_AUTHORIZATION=f"Bearer {access_token}")
-#         response = client.get(url)
-#         assert response.status_code == 200
+@pytest.mark.django_db
+class TestCompany:
+    def test_list_companies_success(self):
+        url = reverse("companies-list")
+        admin = User.objects.create(
+            role=User.UserRoles.Admin,
+            email="admin@gmail.com",
+            name="admin",
+            phone_number="123456789",
+        )
+        admin.set_password("admin")
+        admin.save()
+        access_token = AccessToken.for_user(admin)
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION=f"Bearer {access_token}")
+        response = client.get(url)
+        assert response.status_code == 200
 
-#     def test_list_companies_with_district_filter(self):
-#         admin = User.objects.create(
-#             role=User.UserRoles.Admin,
-#             email="admin@gmail.com",
-#             name="admin",
-#             phone_number="123456789",
-#         )
-#         admin.set_password("admin")
-#         admin.save()
-#         access_token = AccessToken.for_user(admin)
-#         client = APIClient()
-#         client.credentials(HTTP_AUTHORIZATION=f"Bearer {access_token}")
-#         egypt_country = Country.objects.create(name="Egypt", code="EGP")
-#         city = City.objects.create(name="Assuit", country_id=egypt_country.id)
-#         district = District.objects.create(name="Cairo", city=city)
-#         url = reverse("companies-list")
-#         response = client.get(url, {"district": district.id})
-#         assert response.status_code == 200
+    def test_list_companies_with_district_filter(self):
+        admin = User.objects.create(
+            role=User.UserRoles.Admin,
+            email="admin@gmail.com",
+            name="admin",
+            phone_number="123456789",
+        )
+        admin.set_password("admin")
+        admin.save()
+        access_token = AccessToken.for_user(admin)
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION=f"Bearer {access_token}")
+        egypt_country = Country.objects.create(name="Egypt", code="EGP")
+        city = City.objects.create(name="Assuit", country_id=egypt_country.id)
+        district = District.objects.create(name="Cairo", city=city)
+        url = reverse("companies-list")
+        response = client.get(url, {"district": district.id})
+        assert response.status_code == 200
 
-#     def test_list_company_with_district_filter_falied(self):
-#         pass
+    def test_list_company_with_district_filter_falied(self):
+        pass
 
 
 @pytest.mark.django_db
