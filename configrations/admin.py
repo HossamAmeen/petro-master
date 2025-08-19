9af9ea1e1dacabf5de0fd006dab6d0131e2c942c
Hossam <hossam.mohamed@shelter-technology.com>
2025-08-19 18:18:34 +0300

    add slider in admin panel

--- configrations/admin.py
+++ configrations/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import ConfigrationsModel
+from .models import ConfigrationsModel, Slider
 
 # Register your models here.
 
 
 admin.site.register(ConfigrationsModel)
+
+admin.site.register(Slider)
