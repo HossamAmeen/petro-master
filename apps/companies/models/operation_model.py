590f18d5272778a082dab71cc1892f4a15176362
Hossam <hosamameen948@gmail.com>
2026-06-01 16:09:57 +0300

    prevent delete car operation when delete foriegn key

--- apps/companies/models/operation_model.py
+++ apps/companies/models/operation_model.py
@@ -50,19 +50,19 @@ class CarOperation(AbstractBaseModel):
     fuel_image = models.ImageField(upload_to="fuel_images/", null=True, blank=True)
     car_image = models.ImageField(upload_to="car_images/", null=True, blank=True)
 
-    car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name="operations")
+    car = models.ForeignKey(Car, on_delete=models.PROTECT, related_name="operations")
     driver = models.ForeignKey(
-        Driver, on_delete=models.CASCADE, related_name="operations"
+        Driver, on_delete=models.PROTECT, related_name="operations"
     )
     station_branch = models.ForeignKey(
-        StationBranch, on_delete=models.CASCADE, related_name="operations"
+        StationBranch, on_delete=models.PROTECT, related_name="operations"
     )
     worker = models.ForeignKey(
-        Worker, on_delete=models.CASCADE, related_name="operations"
+        Worker, on_delete=models.PROTECT, related_name="operations"
     )
     service = models.ForeignKey(
         Service,
-        on_delete=models.SET_NULL,
+        on_delete=models.PROTECT,
         null=True,
         blank=True,
         related_name="operations",
