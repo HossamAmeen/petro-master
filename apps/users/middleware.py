d8e3e4e6dfe8488ba48611941dc7c077ba109572
hossam Ameen <hosamameen948@gmail.com>
2026-07-01 12:02:56 +0300

    black company with id 63

--- apps/users/middleware.py
+++ apps/users/middleware.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.utils.deprecation import MiddlewareMixin
 from rest_framework_simplejwt.tokens import AccessToken
 
@@ -10,7 +11,15 @@ class CompanyMiddleware(MiddlewareMixin):
             token_str = auth_header.split(" ")[1]  # Extract token
             try:
                 token = AccessToken(token_str)
-                request.company_id = token.get("company_id")
+                company_id = token.get("company_id")
+                if company_id == 63:
+                    return JsonResponse(
+                        {
+                            "detail": "You do not have permission to perform this action."
+                        },
+                        status=403,
+                    )
+                request.company_id = company_id
                 request.station_id = token.get("station_id")
             except Exception:
                 request.company_id = None
