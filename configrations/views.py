209dff16757af7931584e6f094ef89b637171a4d
Hossam <hosamameen948@gmail.com>
2026-08-03 15:15:06 +0300

    fix import

--- configrations/views.py
+++ configrations/views.py
@@ -5,7 +5,7 @@ from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from apps.configrations.throttles import ContactUsRateThrottle
+from configrations.throttles import ContactUsRateThrottle
 
 from apps.accounting.models import (
     CompanyKhaznaTransaction,
